54b2e877f94e32a69b8036e503f775f05949a0b1
antblessing <antblessing@gmail.com>
2016-03-25 14:34:29 -0700

    Only keep track of @top

--- Stack.rb
+++ Stack.rb
@@ -1,72 +1,51 @@
 class Stack
   def initialize
-    @head = nil
-    @head = @tail
+    @top = nil
   end
 
   def is_empty?
-  	if @head == nil
-  		true
+  	return false if @top
+    true
+  end
+
+  def search(data)
+  	if @top == nil
+  		puts "Stack is empty."
   	else
-  		false
+  		current_node = @top
+  		offset = 0
+  		until current_node.data == data
+  			current_node = current_node.previous_node
+  			offset += 1
+  			if current_node.previous_node == nil && current_node.data != data
+  				return ".1"
+  			end
+  		end
+  		return "Item is #{offset} from the top of the stack."
   	end
   end
 
   def peek
-  	if @head == nil
-  		puts "Stack is empty."
-  	else
-  		current_node = @head
-  		while current_node.next_node != nil
-        current_node = current_node.next_node
-      end
-      return current_node
-    end
+  	return @top.data
   end
 
   def push(data)
-  	if @head == nil
-      @head = Node.new(data)
-    else
-      current_node = @head
-      while current_node.next_node != nil
-        current_node = current_node.next_node
-      end
-    current_node.next_node = Node.new(data)
-    @tail = current_node.next_node
-    end
-  end
-
-  def pop
-  	if @head == nil
-  		puts "Stack is empty."
+  	if @top == nil
+  		@top = Node.new(data)
   	else
-  		current_node = @head
-  		while current_node.next_node != @tail
-  			current_node = current_node.next_node
-  		end
-  		current_node.next_node = nil
-  		@tail = current_node
+  		new_node = Node.new(data)
+  		new_node.previous_node = @top
+  		@top = new_node
   	end
   end
 
-  def search(data)
-  	if @head == nil
+  def pop
+  	if @top == nil
   		puts "Stack is empty."
   	else
-  		current_node = @head
-  		offset = 0
-  		until current_node.data == data
-  			current_node = current_node.next_node
-  			if current_node.next_node == nil
-  				return ".1"
-  			end
-  		end
-  		while current_node.next_node != nil
-  			current_node = current_node.next_node
-  			offset += 1
-  		end
-  		return "Item is #{offset + 1} from the top of the stack."
+  		current_node = @top
+  		@top = current_node.previous_node
+  		current_node = nil
   	end
   end
 end
