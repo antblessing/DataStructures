2f62f1ecbf32dde7101d2a2e05c996d8ed02ecbb
antblessing <antblessing@gmail.com>
2016-04-06 14:34:32 -0700

    queue methods

--- Queue.rb
+++ Queue.rb
@@ -1,9 +1,55 @@
+require 'stack'
 class Queue
-  require 'Stack'
-  def initialize
-    stack_in = Stack.new
-    stack_out = Stack.new
-  end
-  def add(element)
-  end
+	def initialize
+		@stack_in = Stack.new
+		@stack_out = Stack.new
+	end
+
+	def empty?
+		return true if @stack_in.is_empty?
+		false
+	end
+	def add(data)
+		@stack_in.push(data)
+	end
+
+	def size
+		counter = 0
+		until @stack_in.is_empty?
+			@stack_out.push(@stack_in.peek)
+			@stack_in.pop
+			counter += 1
+		end
+
+		until @stack_out.is_empty?
+			@stack_in.push(@stack_out.peek)
+			@stack_out.pop
+		end
+		return counter
+	end
+
+	def find_head
+		until @stack_in.is_empty?
+			@stack_out.push(@stack_in.peek)
+			@stack_in.pop
+		end
+		return head = @stack_out.peek
+		until @stack_out.is_empty?
+			@stack_in.push(@stack_out.peek)
+			@stack_out.pop
+		end
+	end
+
+	def remove
+		until @stack_in.is_empty?
+			@stack_out.push(@stack_in.peek)
+			@stack_in.pop
+		end
+		@stack_out.pop
+		@head = @stack_out.peek
+		until @stack_out.is_empty?
+			@stack_in.push(@stack_out.peek)
+			@stack_out.pop
+		end
+	end
 end
